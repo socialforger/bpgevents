277da22a8fc2fae636fa4cceff0fdf2605d01627
Socialforger <socialforger@gmail.com>
2025-12-31 10:22:48 +0100

    Update class-bpgevents-markers.php

--- includes/class-bpgevents-markers.php
+++ includes/class-bpgevents-markers.php
@@ -2,7 +2,32 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 class BPGEVENTS_Markers {
-    public function __construct() {
-        // Placeholder per future marker customization
+
+    /**
+     * URL marker standard
+     */
+    public static function get_default_marker() {
+        return plugin_dir_url( dirname( __FILE__ ) ) . 'assets/img/marker-default.png';
+    }
+
+    /**
+     * URL shadow marker
+     */
+    public static function get_default_shadow() {
+        return plugin_dir_url( dirname( __FILE__ ) ) . 'assets/img/marker-shadow.png';
+    }
+
+    /**
+     * URL icona evento virtuale
+     */
+    public static function get_virtual_icon() {
+        return plugin_dir_url( dirname( __FILE__ ) ) . 'assets/img/icon-virtual.png';
+    }
+
+    /**
+     * URL icona evento presenziale
+     */
+    public static function get_presential_icon() {
+        return plugin_dir_url( dirname( __FILE__ ) ) . 'assets/img/icon-presential.png';
     }
 }
